2aafa9e022ca5311fb1bdc81d1063bb6677ef6b5
GiselliR <gisellidasilva32@gmail.com>
2023-10-23 20:33:00 -0300

    Finalizando a estrutura e fazendo as configurações iniciais

--- Django/TCC_Polaris/setup/settings.py
+++ Django/TCC_Polaris/setup/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'WebSite',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'polarisBD',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
