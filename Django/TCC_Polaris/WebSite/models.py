468cc405c2bf5beed1bbfb827328adc893d3649e
GiselliR <gisellidasilva32@gmail.com>
2023-10-23 20:59:04 -0300

    Configurei a base dos models Comentarios, Like e Fotos

--- Django/TCC_Polaris/WebSite/models.py
+++ Django/TCC_Polaris/WebSite/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Comentarios_BD(models.Model):
+    comentario = models.TextField(max_length=255)
+    data_comentario = models.DateField()
+    autor = models.ForeignKey(User.username)
+
+class Like_BD(models.Model):
+    like = models.IntegerField()
+    autor = models.ForeignKey(User.username)
+
+class Fotos_BD(models.Model):
+    titulo = models.CharField(max_length=25)
+    descricao = models.TextField(max_length=255)
+    autor = models.ForeignKey(User.username)
+    
