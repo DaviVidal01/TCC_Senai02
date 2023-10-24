75e6584e09e1b98b3dcfa8d8de3a4fb8175b1932
GiselliR <gisellidasilva32@gmail.com>
2023-10-23 22:05:17 -0300

    Definindo urls e views do index

--- Django/TCC_Polaris/WebSite/views.py
+++ Django/TCC_Polaris/WebSite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Like_BD, Fotos_BD, Comentarios_BD
 
-# Create your views here.
+def index(request):
+    likes_view = Like_BD.objects.all()
+    fotos_view = Fotos_BD.objects.all()
+    comentarios_view = Comentarios_BD.objects.all()
+    return render(request, 'index.html', {'likes': likes_view,'fotos': fotos_view,'comentarios': comentarios_view})
+
