c434e93cb53680fe4ac3d66f0646d43b532c9037
GiselliR <gisellidasilva32@gmail.com>
2023-10-24 22:17:07 -0300

    Configurando Models e personalizando Admin Django

--- Django/TCC_Polaris/WebSite/admin.py
+++ Django/TCC_Polaris/WebSite/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Like_BD)
-admin.site.register(Comentarios_BD)
-admin.site.register(Fotos_BD)
+@admin.register(Like_BD)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('like', 'autor')
+
+@admin.register(Comentarios_BD)
+class ComentariosAdmin(admin.ModelAdmin):
+    list_display = ('comentario', 'autor', 'data_comentario')
+    list_filter = ('autor', 'data_comentario')
+
+@admin.register(Fotos_BD)
+class FotosAdmin(admin.ModelAdmin):
+    search_fields = ('titulo', 'autor')
+    list_display = ('titulo', 'autor', 'data_foto')
+    list_filter = ('autor', 'data_foto')
+    fieldsets = [
+        ('Informações Básicas', {'fields': ['titulo', 'autor', 'data_foto']}),
+        ('Detalhes Adicionais', {'fields': ['images']}),
+    ]
--- Django/TCC_Polaris/WebSite/models.py
+++ Django/TCC_Polaris/WebSite/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
+
 # Create your models here.
 
 class Comentarios_BD(models.Model):
     comentario = models.TextField(max_length=255)
-    data_comentario = models.DateField()
+    data_comentario = models.DateField(default='')
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Like_BD(models.Model):
@@ -14,6 +16,7 @@ class Like_BD(models.Model):
 class Fotos_BD(models.Model):
     titulo = models.CharField(max_length=25)
     descricao = models.TextField(max_length=255)
-    foto = models.ImageField(default='', upload_to= './images')
+    data_foto = models.DateField(default=datetime.today)
+    foto = models.ImageField(default=datetime.today, upload_to= './images')
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     
