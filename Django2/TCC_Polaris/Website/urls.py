35889280229858888824a8c642c3003f5082aa99
DaviVidal01 <davividal0605@gmail.com>
2023-11-06 08:03:07 -0300

    Organizando dashboard em partials

--- Django2/TCC_Polaris/Website/urls.py
+++ Django2/TCC_Polaris/Website/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('detalhes_fotos/', views.detalhes_fotos, name='detalhes_fotos'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('add_fotos/', views.add_fotos, name='add_fotos'),
 ]
