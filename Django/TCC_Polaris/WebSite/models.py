ec73aeb9100874710bdbd3c7d382fadcad1d426c
luquinha157 <131728136+luquinha157@users.noreply.github.com>
2023-11-06 23:11:13 -0300

    Update models.py

--- Django/TCC_Polaris/WebSite/models.py
+++ Django/TCC_Polaris/WebSite/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
+from django.utils import timezone
 
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Comentarios_BD(models.Model):
-    comentario = models.TextField(max_length=255)
-    data_comentario = models.DateField(default='')
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario = models.TextField()
+    data_comentario = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"Comentário por {self.autor} em {self.foto.titulo}"
 
 class Like_BD(models.Model):
     like = models.IntegerField()
@@ -16,7 +20,10 @@ class Like_BD(models.Model):
 class Fotos_BD(models.Model):
     titulo = models.CharField(max_length=25)
     descricao = models.TextField(max_length=255)
-    data_foto = models.DateField(default=datetime.today)
-    foto = models.ImageField(default=datetime.today, upload_to= './images')
+    data_foto = models.DateField(default=timezone.now)
+    foto = models.ImageField(upload_to= 'images/')
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
+#-----Imagem da Barra de Pesquisa
+class Barra_Pesquisa(models.Model):
+    imagem = models.ImageField(upload_to= 'images/')
