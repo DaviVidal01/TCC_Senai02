3132299420a5e5eb128ee3c940499aa982bab485
lucas <lucasfariamelo307@gmail.com>
2023-11-27 09:25:04 -0300

    Criando o botão de comprar

--- Models/Paginate/Paginate/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.http import HttpResponse
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('fotos/', views.fotosDoCatalogo, name='fotos'),
-]
