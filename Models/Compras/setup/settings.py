d3d650bd4d43ba41897a1896c3470a89bbe999fe
Nixx693 <nathaliafedoci@gmail.com>
2023-12-04 21:21:09 -0300

    atualização descrição e preço das peças

--- Models/Compras/setup/settings.py
+++ Models/Compras/setup/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-import pymysql
-pymysql.install_as_MySQLdb()
-pymysql = "==1.1.0"
+#import pymysql
+#pymysql.install_as_MySQLdb()
+#pymysql = "==1.1.0"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
