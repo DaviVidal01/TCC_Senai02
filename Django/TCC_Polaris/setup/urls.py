993111b700100eefbc77ad778eb0ad1a8aa3efcb
Kali <trabalho.computador.sjn@gmail.com>
2023-12-14 19:08:08 -0300

    foi alterado nome de url

--- Django/TCC_Polaris/setup/urls.py
+++ Django/TCC_Polaris/setup/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Website.urls'))
+    path('', include('WebSite.urls'))
 ]
 
 if settings.DEBUG:
